8f016a90849072cf637c0b2cdc60d4a12d4bead6
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-09-13 22:35:32 -0400

    Agregados archivos iniciales

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Databases with php</title>
+    <!-- download css framework: http://getbootstrap.com/ -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+        <h1>Databases</h1>
+        <ul>
+          <li><a href="">List users</a></li>
+          <li><a href="">Add users</a></li>
+        </ul>
+      </div>
+  </body>
+</html>
